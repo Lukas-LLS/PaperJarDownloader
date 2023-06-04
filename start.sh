8944996bc6c8f614db7edbeb36c8295a04873c5d
Lukas-LLS <lukas.lorenz.spilka@gmail.com>
2023-06-05 00:01:06 +0200

    Attempt to fix set timezone

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:/opt/java/openjdk/bin
-su ln -sf /usr/share/zoneinfo/$(curl -s 'http://worldtimeapi.org/api/ip' | jq -r '.timezone') /etc/localtime
+su -c "ln -sf /usr/share/zoneinfo/$(curl -s 'http://worldtimeapi.org/api/ip' | jq -r '.timezone') /etc/localtime" root
 java -jar ./PaperJarDownloader.jar
 cd ./server || exit
 echo eula=true > eula.txt
