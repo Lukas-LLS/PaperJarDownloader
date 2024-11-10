ceea15a180c9ebe9d7d4ff86d9213f0be1ade84e
Lukas-LLS <56191752+Lukas-LLS@users.noreply.github.com>
2024-11-10 15:04:33 +0100

    Fix timezone bug in start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:/opt/java/openjdk/bin
 TZ=$(curl -s 'http://worldtimeapi.org/api/ip' | jq -r '.timezone')
-export TZ
+export $TZ
 java -jar ./PaperJarDownloader.jar
 cd ./server || exit
 echo eula=true > eula.txt
