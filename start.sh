7ce6944616bdba0b1ed91557a55eec92c5af2748
Lukas-LLS <lukas.lorenz.spilka@gmail.com>
2024-11-13 14:39:40 +0100

    Fix timezone bug when service is unreachable and switch mc server to use generational ZGC

--- start.sh
+++ start.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
 export PATH=$PATH:/opt/java/openjdk/bin
-TZ=$(curl -s 'http://worldtimeapi.org/api/ip' | jq -r '.timezone')
-export $TZ
+
+TZ=$(curl -s "http://worldtimeapi.org/api/ip" | jq -r ".timezone")
+
+if [ -z "$TZ" ]; then
+  IP=$(curl -s "http://ifconfig.me/ip")
+  TZ=$(curl -s "https://timeapi.io/api/timezone/ip?ipAddress=$IP" | jq -r ".timeZone")
+fi
+
+if [ -z "$TZ" ]; then
+  echo "Failed to get timezone, using default timezone"
+  TZ="UTC"
+fi
+
+export TZ
+echo "Timezone is $TZ"
+
 java -jar ./PaperJarDownloader.jar
 cd ./server || exit
 echo eula=true > eula.txt
-java -jar ./server.jar nogui
+java -jar -XX:+UseZGC -XX:+ZGenerational ./server.jar nogui
