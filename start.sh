77ba729a98cd1d7c2cd8d4ea697ac8ff62bbc5d1
Lukas-LLS <lukas.lorenz.spilka@gmail.com>
2025-03-07 09:29:37 +0100

    Allow presetting the timezone in the docker container

--- start.sh
+++ start.sh
@@ -2,16 +2,20 @@
 
 export PATH=$PATH:/opt/java/openjdk/bin
 
-TZ=$(curl -s "http://worldtimeapi.org/api/ip" | jq -r ".timezone")
-
 if [ -z "$TZ" ]; then
-  IP=$(curl -s "http://ifconfig.me/ip")
-  TZ=$(curl -s "https://timeapi.io/api/timezone/ip?ipAddress=$IP" | jq -r ".timeZone")
-fi
+  TZ=$(curl -s "http://worldtimeapi.org/api/ip" | jq -r ".timezone")
 
-if [ -z "$TZ" ]; then
-  echo "Failed to get timezone, using default timezone"
-  TZ="UTC"
+  if [ -z "$TZ" ]; then
+    IP=$(curl -s "http://ifconfig.me/ip")
+    TZ=$(curl -s "https://timeapi.io/api/timezone/ip?ipAddress=$IP" | jq -r ".timeZone")
+  fi
+
+  if [ -z "$TZ" ]; then
+    echo "Failed to get timezone, using default timezone"
+    TZ="UTC"
+  fi
+else
+  echo "Timezone is preset to $TZ"
 fi
 
 export TZ
