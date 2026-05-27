f529b735121679f1d713d73c2c0d65f80902737f
Lukas-LLS <lukas.lorenz.spilka@gmail.com>
2026-05-27 10:42:57 +0200

    Enable gradle configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,20 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+	id("com.gradle.develocity") version "4.4.2"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
+    }
 }
 
 rootProject.name = "PaperJarDownloader"
